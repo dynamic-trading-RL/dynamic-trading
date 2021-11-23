5a24b05c113622c4ee8ca8b74b5acc828e5736a4
federico.giorgi@arpm.co <federico.giorgi@arpm.co>
2021-11-23 15:07:42 +0100

    erased files

--- run-nonlinear.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-#SBATCH --job-name=trading
-#SBATCH --cpus-per-task=80
-python nonlinear_series.py
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-#SBATCH --job-name=trading
-#SBATCH --cpus-per-task=80
-python get_time_series.py && python train_RL_agent.py && python back_testing.py && python out_of_sample.py
