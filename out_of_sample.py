59c8ed558caa44a8b47acf7c7d6103ab29843443
Federico Giorgi <federico.giorgi@postevita.it>
2021-12-20 11:21:47 +0100

    decreas oos simulations

--- out_of_sample.py
+++ out_of_sample.py
@@ -27,7 +27,7 @@ if not sys.warnoptions:
 # ------------------------------------- Parameters ----------------------------
 
 
-j_oos = 15000
+j_oos = 100
 t_ = 50
 
 returnDynamicsType = ReturnDynamicsType.Linear
