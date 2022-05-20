fbcc543b4de62ddbcb9ab8186d2b8673f19d6a02
Federico Giorgi <federico.giorgi@postevita.it>
2022-05-20 15:27:44 +0200

    reorganized some files

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 #
 #SBATCH --job-name=tr-new
 #SBATCH --cpus-per-task=20
-python scripts/s_train_agent.py && python scripts/s_in_sample_tests.py
+python s_train_agent.py && python s_in_sample_tests.py
