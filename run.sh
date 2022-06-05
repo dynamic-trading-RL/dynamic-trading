3cbaf6db51820ade740ebe62acc80c1f4407e2c3
federico.giorgi <gfederico9119@gmail.com>
2022-06-05 11:55:33 +0200

    run.sh cannot calibrate market dynamics atm because emmy does not have package arch installed

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 #
 #SBATCH --job-name=tr-new
 #SBATCH --cpus-per-task=10
-python s_calibrate_market_dynamics.py && python s_train_agent.py && python s_backtesting.py
+python s_train_agent.py && python s_backtesting.py
