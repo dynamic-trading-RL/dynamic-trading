ca1322b2a69953520d2bb3c53537f6d1c19a0c33
Federico Giorgi <federico.giorgi@postevita.it>
2022-07-22 11:06:39 +0200

    Introduced EstimateInitializationType(Enum)

--- enums.py
+++ enums.py
@@ -33,12 +33,20 @@ class RiskDriverType(Enum):
     PnL = 'PnL'
     Return = 'Return'
 
+
 class FactorSourceType(Enum):
 
     Constructed = 'Constructed'
     Exogenous = 'Exogenous'
 
+
 class ModeType(Enum):
 
     InSample = 'InSample'
     OutOfSample = 'OutOfSample'
+
+class EstimateInitializationType(Enum):
+
+    RandomUniform = 'RandomUniform'
+    RandomTruncNorm = 'RandomTruncNorm'
+    GP = 'GP'
