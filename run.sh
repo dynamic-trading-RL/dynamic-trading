278f64f9bf7ccfc4512beba82d707ddcaacd69a2
Federico Giorgi <federico.giorgi@postevita.it>
2022-07-22 11:07:12 +0200

    run.sh instantiated; the others deleted

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
-#SBATCH --job-name=tr-bmk
+#SBATCH --job-name=tr
 #SBATCH --cpus-per-task=40
 python s_train_agent.py && python s_backtesting.py
--- run_alternative.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-#SBATCH --job-name=tr-alt
-#SBATCH --cpus-per-task=40
-python s_train_agent.py && python s_backtesting.py
