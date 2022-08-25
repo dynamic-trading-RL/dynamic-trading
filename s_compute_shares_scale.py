7f7bcaf47730135bf06ed2cb389d5c2b3aa38193
federico.giorgi <gfederico9119@gmail.com>
2022-08-25 14:28:53 +0200

    Added script that helps determining a good M rescaling factor

---
+++ s_compute_shares_scale.py
@@ -0,0 +1,43 @@
+import numpy as np
+from tqdm import tqdm
+
+from benchmark_agents.agents import AgentMarkowitz
+from market_utils.market import read_trading_parameters_market, instantiate_market
+
+if __name__ == '__main__':
+
+    ticker = 'WTI'
+    j_episodes = 10000
+    t_ = 50
+
+    riskDriverDynamicsType, factorDynamicsType, riskDriverType, factorType = read_trading_parameters_market(ticker)
+    market = instantiate_market(riskDriverDynamicsType=riskDriverDynamicsType,
+                                factorDynamicsType=factorDynamicsType,
+                                ticker=ticker,
+                                riskDriverType=riskDriverType)
+    market.simulate_market_trading(n=0, j_episodes=j_episodes, t_=t_)
+
+    agentMarkowitz = AgentMarkowitz(market=market)
+
+    rescaled_trade_lst = []
+    rescaled_shares_lst = []
+
+    for j in tqdm(range(j_episodes), desc='Computing trajectories of Markowitz strategy'):
+
+        current_rescaled_shares = 0.
+
+        for t in range(t_):
+
+            current_factor = market.simulations['factor'][j, t]
+
+            rescaled_trade = agentMarkowitz.policy(current_factor=current_factor,
+                                                   current_rescaled_shares=current_rescaled_shares)
+
+            current_rescaled_shares += rescaled_trade
+
+            rescaled_trade_lst.append(rescaled_trade)
+            rescaled_shares_lst.append(current_rescaled_shares)
+
+    M = int(np.quantile(a=np.abs(np.array(rescaled_shares_lst)), q=0.99))
+
+    print(f'M = {M}')
