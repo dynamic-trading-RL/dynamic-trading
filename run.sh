5c39f9e1bc93755c0a1bd772bf1ca1e27907fc03
Federico Giorgi <federico.giorgi@postevita.it>
2022-10-05 10:41:04 +0200

    reverted tests

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@
 #
 #SBATCH --job-name=tr
 #SBATCH --cpus-per-task=8
-conda activate /home/giorgi/conda/env_py3_10/
 python s_calibrate_specific_market_dynamics.py
 python s_compute_shares_scale.py
 python s_train_agent.py
