6f03a043ccc47b83acfd8e2ebde37a74bcbbc08a
Federico Giorgi <federico.giorgi@postevita.it>
2022-11-04 16:24:06 +0100

    Minor tweaks

--- s_train_agent.py
+++ s_train_agent.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     # if True, the agent uses the model to predict the next step pnl and sig2 for the reward; else, uses the realized
     predict_pnl_for_reward = False
     # if True, the agent averages across supervised regressors in its definition of q_value; else, uses the last one
-    average_across_models = False
+    average_across_models = True
     # if True, then the agent considers the supervised regressors only up to n<=n_batches, where n is the batch that
     # provided the best reward in the training phase
     use_best_n_batch = True
