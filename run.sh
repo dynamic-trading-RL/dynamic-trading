e00ffc54876770411153eab97541e228b5560dec
giorgif8@posteitaliane.it <federico.giorgi@postevita.it>
2023-02-03 11:06:05 +0100

    reset_all_data

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 #
 #SBATCH --job-name=tr
 #SBATCH --cpus-per-task=40
+python s_reset_all_data.py
 python s_calibrate_specific_market_dynamics.py
 python s_compute_shares_scale.py
 python s_train_agent.py
---
+++ s_reset_all_data.py
@@ -0,0 +1,34 @@
+
+import os
+
+if __name__ == '__main__':
+
+    print(__file__)
+    print(os.getcwd())
+
+    folders_2b_emptied = (
+        '/data/data_tmp/',
+        '/data/financial_time_series_data/financial_time_series_calibrations/',
+        '/data/financial_time_series_data/financial_time_series_info/',
+        '/data/supervised_regressors/',
+        '/figures/backtesting/',
+        '/figures/on_the_fly_backtesting/',
+        '/figures/on_the_fly_simulationtesting/',
+        '/figures/polynomial/',
+        '/figures/simulationtesting/',
+        '/figures/training/',
+        '/reports/calibrations/',
+        '/reports/sharpe_ratios',
+        '/reports/t-tests/'
+    )
+
+    for folder in folders_2b_emptied:
+        folderpath = os.path.dirname(__file__) + folder
+        for filename in os.listdir(folderpath):
+            if filename == '.keep':
+                continue
+            try:
+                filepath = folderpath + filename
+                os.remove(filepath)
+            except:
+                continue
