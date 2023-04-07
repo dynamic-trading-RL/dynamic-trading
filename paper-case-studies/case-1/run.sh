650e6b826888f1312a6534616d07a138660a7723
Federico Giorgi <federico.giorgi@postevita.it>
2023-04-07 11:05:06 +0200

    new calibrations

--- paper-case-studies/case-1/run.sh
+++ paper-case-studies/case-1/run.sh
@@ -2,4 +2,4 @@
 #
 #SBATCH --job-name=tr-1
 #SBATCH --cpus-per-task=20
-python train_RL_agent.py && python out_of_sample.py && python backtesting.py
+python fit_dynamics.py && python train_RL_agent.py && python out_of_sample.py && python backtesting.py
--- paper-case-studies/case-3/run.sh
+++ paper-case-studies/case-3/run.sh
@@ -2,4 +2,4 @@
 #
 #SBATCH --job-name=tr-3
 #SBATCH --cpus-per-task=20
-python train_RL_agent.py && python out_of_sample.py && python backtesting.py
+python fit_dynamics.py && python train_RL_agent.py && python out_of_sample.py && python backtesting.py
